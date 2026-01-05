44adfe0ebe385e028a999a82ecae17a93b6461b6
Skanda02 <skandaprasad02@gmail.com>
2026-01-05 22:43:43 +0530

    .

---
+++ Stack_Applications/Evl_suffix_exp/Evl_suffix_Exp.c
@@ -0,0 +1,66 @@
+/*
+Evaluation of Suffix expression with single digit operands: +, -, *, /, %, ^
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>   
+
+int i, top=-1, op1, op2, res, s[20];
+char postfix[90], symb;
+
+void push(int item){
+    s[++top] = item;
+}
+
+int pop(){
+    int item;
+    item = s[top--];
+    return item;
+}
+
+int main()
+{
+    printf("Enter a valid postfix expression:");
+    scanf("%s",postfix);
+
+    for(i=0; postfix[i] != '\0'; i++){
+        symb = postfix[i];
+        if(isdigit(symb)){
+            push(symb-'0');
+        }
+        else{
+            op2 = pop();
+            op1 = pop();
+
+            switch (symb)
+            {
+            case'+':
+                push(op1+op2);
+                break;
+            
+            case'-':
+                push(op1-op2);
+                break;
+
+            case'/':
+               push(op1/op2);
+               break;
+
+            case'%':
+               push(op1%op2);
+               break;
+
+            case'^':
+               push(pow(op1,op2));
+               break;
+
+            default:
+                push(0);
+            }
+        }
+    }
+    res = pop();
+    printf("\n Result = %d\n", res);
+}
